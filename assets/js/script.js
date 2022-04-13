d7c05865b948e0fa553a32efeebf51db779581de
Rene Ortega <reneortega2468@gmail.com>
2022-04-12 20:56:49 -0400

    ayoooo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,16 @@ console.log("ayo?")
 
 const begin = $("#begin")
 
+
+
 // remove load screen
 
 $(begin).on("click", function (e) {
   e.preventDefault()
-  const form = $("#form")
-  form.addClass("hidden")
-
+  const form = $("#form");
+  form.addClass("hidden");
+  const info = $("#info");
+  if (info.hasClass == "none"){
+    info.addClass("visible");
+  }
 });

e43122d311e554642b61cead43b8543b99692b1d
Rene Ortega <reneortega2468@gmail.com>
2022-04-12 21:30:34 -0400

    workin js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,13 @@ const begin = $("#begin")
 
 // remove load screen
 
-$(begin).on("click", function (e) {
-  e.preventDefault()
-  const form = $("#form");
-  form.addClass("hidden");
-  const info = $("#info");
-  if (info.hasClass == "none"){
-    info.addClass("visible");
-  }
-});
+$(begin).on('click', function (e) {
+   e.preventDefault();
+   const form = $('#form');
+   form.addClass('hidden');
+   document.getElementById('info').style.display = 'block';
+  // const info = $("#info");
+  // if (info.hasClass == "none"){
+  //   info.addClass("visible");
+  // }
+ });

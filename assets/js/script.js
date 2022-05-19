4d9d70540f1d942510cdf1fb72bed6ab210d9ca9
rene <rinipini@Rinis-MBP.attlocal.net>
2022-05-19 18:36:06 -0400

    pushing to PC

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,29 @@ $(begin).on('click', function (e) {
     document.getElementById('projects').style.display = 'grid';
 
 });
+
+let form = document.getElementById("contact");
+form.addEventListener(
+    "submit",
+    function (event) {
+        event.preventDefault();
+        let elements = form.elements;
+        let payload = {};
+        for (let i = 0; i < elements.length; i++) {
+            let item = elements.item(i);
+            switch (item.type) {
+                case "checkbox":
+                    payload[item.name] = item.checked;
+                    break;
+                case "submit":
+                    break;
+                default:
+                    payload[item.name] = item.value;
+                    break;
+            }
+        }
+        // Place your API call here to submit your payload.
+        // console.log("payload", payload);
+    },
+    true
+);

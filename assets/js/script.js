b3bd2a2b10f93499a54e7b3e322edd9eb3d4f32d
Rene Ortega <reneortega2468@gmail.com>
2022-05-19 20:29:11 -0400

    Added a email message form, and new domai

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,28 +15,28 @@ $(begin).on('click', function (e) {
 
 });
 
-let form = document.getElementById("contact");
-form.addEventListener(
-    "submit",
-    function (event) {
-        event.preventDefault();
-        let elements = form.elements;
-        let payload = {};
-        for (let i = 0; i < elements.length; i++) {
-            let item = elements.item(i);
-            switch (item.type) {
-                case "checkbox":
-                    payload[item.name] = item.checked;
-                    break;
-                case "submit":
-                    break;
-                default:
-                    payload[item.name] = item.value;
-                    break;
-            }
-        }
-        // Place your API call here to submit your payload.
-        // console.log("payload", payload);
-    },
-    true
-);
+// let form = document.getElementById("contact");
+// form.addEventListener(
+//     "submit",
+//     function (event) {
+//         event.preventDefault();
+//         let elements = form.elements;
+//         let payload = {};
+//         for (let i = 0; i < elements.length; i++) {
+//             let item = elements.item(i);
+//             switch (item.type) {
+//                 case "checkbox":
+//                     payload[item.name] = item.checked;
+//                     break;
+//                 case "submit":
+//                     break;
+//                 default:
+//                     payload[item.name] = item.value;
+//                     break;
+//             }
+//         }
+//         // Place your API call here to submit your payload.
+//         // console.log("payload", payload);
+//     },
+//     true
+// );
